90afb2f5adbda1aba6a1168238d5c5b8a397fff5
Elijah White <131682732+1ElijahW@users.noreply.github.com>
2023-06-21 12:07:34 -0400

    missing post.js found

--- frontend-app-dogfriends/src/components/Posts.jsx
+++ frontend-app-dogfriends/src/components/Posts.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
 
-function Posts() {
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // Fetch posts from the API using Axios
+    axios.get('https://dog-friends-backend-40a099a23239.herokuapp.com/api/posts')
+      .then(response => {
+        setPosts(response.data);
+      })
+      .catch(error => console.error('Error fetching posts:', error));
+  }, []);
+
   return (
-    <div>
-      <div className='Posts1'>
-        <div>Sup mate</div>
-      </div>
-
-      <div className='Posts2'>
-        <div>Got cash?</div>
-      </div>
-
-      <div className='Posts3'>
-        <div>#AtThePark..</div>
-      </div>
+    <div className="posts-container">
+      {posts.map(post => (
+        <div className="post" key={post._id}>
+          <img src={post.photo} alt="Dog" />
+          <p>{post.text}</p>
+          <small>{new Date(post.date).toLocaleString()}</small> {/* Display the date in a readable format */}
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default Posts;
+
+
