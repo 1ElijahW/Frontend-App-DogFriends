c6650e45ce5c473fb51cc40854c46d862aaef1db
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-06-21 09:34:59 -0400

    Added auth to frontend, needs testing

--- frontend-app-dogfriends/src/App.js
+++ frontend-app-dogfriends/src/App.js
@@ -9,19 +9,26 @@ import Profile from './pages/Profile';
 import Sidebar from './components/Sidebar';
 import Settings from './pages/Settings';
 import About from './pages/About';
-
+import { AuthContext } from './context/AuthContextComponent';
 
 function App() {
+  const { isLoggedIn } = useContext(AuthContext)
+  console.log(isLoggedIn)
   return (
     <div className='App'>
     <Sidebar/>
     <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/Profile" element={<Profile/>}/>
+    <Route path="/auth/signup" element={<Signup/>} />
+    <Route path="/auth/signin" element={<Signin/>} />
+    { isLoggedIn && 
+          <>
     <Route path="/NewPost" element={<NewPost/>}/>
     <Route path="/Settings" element={<Settings/>}/>
     <Route path="/About" element={<About/>}/>
-
+    </>
+  }
     </Routes>
     </div>
   );
---
+++ frontend-app-dogfriends/src/context/AuthContextComponent.jsx
@@ -0,0 +1,26 @@
+import React, { createContext, useEffect, useState } from 'react'
+import { isTokenValid } from '../api/auth'
+
+// Create a Context Provider called AuthProvider
+export const AuthContext = createContext(null)
+
+// Create our own Context component in which data about auth lives on its state
+export default function AuthContextComponent({children}) {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [user, setUser] = useState({})
+
+    // Write some logic to onload check if the user is logged in, then set loggen in accordingly.
+    useEffect(() => {
+        isTokenValid().then((response) => setIsLoggedIn(response.valid) )
+    }, [])
+
+    // AuthContext, will simply render the AuthProvider, 
+    // and pass its state data into it, so other components can use it.
+    return (
+        <AuthContext.Provider value={
+            { isLoggedIn, setIsLoggedIn, user }
+        }>
+            {children}
+        </AuthContext.Provider>
+    )
+}
